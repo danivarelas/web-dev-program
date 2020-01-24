b8cc0ab216949b4bc62ac4f9e3a5b1f8621bd65a
daniel varela serrano <danivarelas@hotmail.com>
2020-01-23 19:12:07 -0600

    pokemon with css

--- week 3/hangman-js/pokemon.js
+++ week 3/hangman-js/pokemon.js
@@ -1,5 +1,6 @@
-//let id = localStorage.getItem('id');
+let id = localStorage.getItem('id');
 let name = document.querySelector(".name");
+let number = document.querySelector(".number");
 let height = document.querySelector(".height");
 let weight = document.querySelector(".weight");
 let sprite = document.querySelector(".sprite");
@@ -7,7 +8,7 @@ let abilities = document.querySelector(".abilities");
 let moves = document.querySelector(".moves");
 let stats = document.querySelector(".stats");
 
-getPokemonInfo(1);
+getPokemonInfo(id);
 
 function getPokemonInfo(id) {
     let pokemon = fetch('https://pokeapi.co/api/v2/pokemon/'+id);
@@ -15,11 +16,14 @@ function getPokemonInfo(id) {
         return response.json()
     }).then(result => {
         name.innerHTML = result.name;
+        name.style.textTransform = "capitalize";
+        number.innerHTML = result.id;
         height.innerHTML = result.height;
         weight.innerHTML = result.weight;
         sprite.src = result.sprites.front_default;
         loadAbilities(result.abilities);
         loadStats(result.stats);
+        loadMoves(result.moves);
     });
 }
 
@@ -27,6 +31,7 @@ function loadAbilities(abilitiesList) {
     abilitiesList.forEach(ability => {
         let li = document.createElement("li");
         li.innerHTML = ability.ability.name;
+        li.style.textTransform = "capitalize";
         abilities.appendChild(li);
     });
 }
@@ -34,7 +39,18 @@ function loadAbilities(abilitiesList) {
 function loadStats(statsList) {
     statsList.forEach(stat => {
         let li = document.createElement("li");
-        li.innerHTML = stat.stat.name + " - " + stat.base_stat;
+        li.innerHTML = stat.stat.name + ": " + stat.base_stat;
+        li.style.textTransform = "capitalize";
         stats.appendChild(li);
     });
+}
+
+function loadMoves(movesList) {
+    for (var i = 0; i < 3; i++) {
+        let randomMove = movesList[Math.floor(Math.random()*movesList.length)];
+        let li = document.createElement("li");
+        li.innerHTML = randomMove.move.name;
+        li.style.textTransform = "capitalize";
+        moves.appendChild(li);
+    }
 }
